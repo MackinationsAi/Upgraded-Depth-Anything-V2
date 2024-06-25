b3368f9b165a06cbb603ee9615bc7e32ef103194
Mackinations.Ai <133395980+MackinationsAi@users.noreply.github.com>
2024-06-25 03:12:28 -0400

    Add files via upload

---
+++ run_image-depth_8bit.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Get the directory of the currently executed script
+set SCRIPT_DIR=%~dp0
+
+REM Navigate to the script directory
+cd /d "%SCRIPT_DIR%"
+
+REM Activate the virtual environment
+CALL venv\Scripts\activate
+
+REM Run the conversion script
+python run_image-depth_8bit.py
